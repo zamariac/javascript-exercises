3e7b7d56be3b2480e20bcc6d1798814a73d6aab3
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-07 21:33:27 -0600

    retirement calc

---
+++ retirementcalculator/scripts/main.js
@@ -0,0 +1,17 @@
+// What is your current age? 25
+// At what age would you like to retire? 65
+// You have 40 years left until you can retire.
+// It's 2015, so you can retire in 2055.
+
+
+// Date.UTC(year, month, day[, hours[, minutes[, seconds[,ms]]]]) 
+
+
+var age = prompt("What is your current age?");
+var retireAge = prompt("At what age would you like to retire?");
+var yearsToRetire = (retireAge - age);
+
+document.write( " You have " + (yearsToRetire) + " years until you can retire!")
+
+
+
