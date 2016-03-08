d8f19ad514703a3f04fa603a5f310a96bfcecb43
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-07 19:55:36 -0600

    initial commit

---
+++ madlib/scripts/main.js
@@ -0,0 +1,8 @@
+var verb = prompt("Now enter a verb!");
+var pluralBodyPart = prompt(" Enter a plural body part!");
+var pastVerbTense = prompt("Enter a verb in past tense!");
+var fruit= prompt("What's your favorite fruit!");
+var amountOfTime = prompt("Enter an amount of time!");
+
+document.write("The day I saw the Monkey King " + (verb) + " was one of the most interesting days of the year. I still cannot believe my "  
++ (pluralBodyPart) + "." + " The Monkey King " + (pastVerbTense) + " one of the largest " + (fruit) + " I had seen in " + (amountOfTime) + ".");
